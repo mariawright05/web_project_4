461d5e850a49ed8de82ed28896003c448c99a542
Maria Wright <maria@mariawrightstudios.com>
2020-06-09 20:10:05 -0400

    feat: javascript &  html temp for initial cards

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-const popup = document.querySelector(".popup");
-const openButton = document.querySelector(".profile__edit-button");
-const closeButton = document.querySelector(".popup__close-button");
-const profileName = document.querySelector(".profile__user-name");
-const profileTitle = document.querySelector(".profile__user-about");
+// Wrappers
+const profilePopup = document.querySelector(".popup_type_edit-profile");
+const cardPopup = document.querySelector(".popup_type_add-card");
 const formElement = document.querySelector(".popup__container");
-const nameInput = document.querySelector(".popup__field_type_name");
-const jobInput = document.querySelector(".popup__field_type_title");
 
-function togglePopup() {
-  popup.classList.toggle("popup_opened");
+// Buttons and other DOM elements
+const editButton = document.querySelector(".profile__edit-button");
+const closeEditButton = profilePopup.querySelector(".popup__close-button");
+
+const addButton = document.querySelector(".profile__add-button");
+const closeAddButton = cardPopup.querySelector(".popup__close-button");
+
+
+const profileName = document.querySelector(".profile__user-name");
+const profileAbout = document.querySelector(".profile__user-about");
+
+//Form functions
+function togglePopup(modal) {
+  modal.classList.toggle("popup_opened");
 }
-
-openButton.addEventListener("click", togglePopup);
-closeButton.addEventListener("click", togglePopup);
 
 formElement.addEventListener("submit", (evt) => {
   evt.preventDefault();
@@ -23,4 +28,97 @@ formElement.addEventListener("submit", (evt) => {
   togglePopup();
 })
 
+// Profile form
+const nameInput = document.querySelector(".popup__field_type_name");
+const jobInput = document.querySelector(".popup__field_type_title");
+
+
+
+editButton.addEventListener("click", () => {
+  togglePopup(profilePopup);
+  console.log("clicked edit button");
+});
+closeEditButton.addEventListener("click", () => {
+  togglePopup(profilePopup);
+});
+
+
+// Add card form
+addButton.addEventListener("click", () => {
+  togglePopup(cardPopup);
+});
+closeAddButton.addEventListener("click", () => {
+  togglePopup(cardPopup);
+});
+
+
+
+
+// Initial card data
+const initialCards = [
+  {
+      name: "Yosemite Valley",
+      link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+      name: "Lake Louise",
+      link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+      name: "Bald Mountains",
+      link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+      name: "Latemar",
+      link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+      name: "Vanois National Park",
+      link: "https://code.s3.yandex.net/web-code/vanois.jpg"
+  },
+  {
+      name: "Lago di Braies",
+      link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+];
+
+const cardTemplate = document.querySelector(".card-template").content.querySelector(".photo-grid__item");
+//const gridItem = document.querySelector(".photo-grid__item");
+const list = document.querySelector(".photo-grid");
+
+
+const createCard = (data) => {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardTitle = cardElement.querySelector(".photo-grid__title");
+  const cardImage = cardElement.querySelector(".photo-grid__image");
+  const cardLikeButton = cardElement.querySelector(".photo-grid__like");
+  const cardRemoveButton = cardElement.querySelector(".photo-grid__remove");
+
+  cardTitle.textContent = data.name;
+  cardImage.style.backgroundImage = `url(${data.link})`;
+
+  cardLikeButton.addEventListener("click", () => {
+    // changeHeartColor()
+  })
+
+  cardRemoveButton.addEventListener("click", () => {
+    // removeCard()
+  })
+
+  cardImage.addEventListener("click", () => {
+    // openPopup
+  })
+
+  return cardElement;
+}
+
+
+const renderCard = (data) => {
+  list.prepend(createCard(data));
+}
+
+
+initialCards.forEach((data) => {
+  renderCard(data);
+})
 
