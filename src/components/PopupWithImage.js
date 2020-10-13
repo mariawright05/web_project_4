3c5c0ddd90b52dcae9bf09484236ce68d744e3b5
Maria Wright <maria@mariawrightstudios.com>
2020-10-13 11:40:24 -0400

    fix: DOM elements moved to global variables

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,19 +3,19 @@ import Popup from "./Popup.js";
 class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+    this._image = this._popupElement.querySelector(".popup__image");
+    this._caption = this._popupElement.querySelector(".popup__image-caption");
   }
 
   open(data) {
     super.open();
     this._link = data.link;
     this._name = data.name;
-    this._popupElement.querySelector(".popup__image").src = this._link;
-    this._popupElement.querySelector(".popup__image").alt = this._name;
-    this._popupElement.querySelector(".popup__image-caption").textContent = this._name;
+    this._image.src = this._link;
+    this._image.alt = this._name;
+    this._caption.textContent = this._name;
   }
 
 }
-
-
 
 export default PopupWithImage;
